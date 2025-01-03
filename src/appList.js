dca9c48d6219a7de62e4f111f9472c56977ccc11
FinalZJY <805642794@qq.com>
2025-01-03 15:27:09 +0800

    fix: create data dir

--- src/appList.js
+++ src/appList.js
@@ -1,8 +1,12 @@
 import SteamAPI from 'steamapi';
 import {writeJSON} from './file.js';
-import {appListFile, steamToken} from './config.js';
+import {appListFile, dataDir, steamToken} from './config.js';
+import fs from 'fs';
 
 const steam = new SteamAPI(steamToken);
 
 const apps =  await steam.getAppList();
+if (!fs.existsSync(dataDir)) {
+  fs.mkdirSync(dataDir);
+}
 writeJSON(appListFile, apps);
